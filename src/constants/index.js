e81b5cb84833ff9d26f1cf1163b2f0fe079cc265
VishalKShady <vishal97shady@gmail.com>
2024-01-30 22:09:48 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,253 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "APIs Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Software Tester",
+      icon: meta,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Python Developer",
+      company_name: "Ciena BluePlanet",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "September 2021 - October 2022",
+      points: [
+        "Developing and maintaining web applications using Python and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "FastAPI Developer",
+      company_name: "Huawei",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "November 2022 - June 2023",
+      points: [
+        "As a seasoned developer with a strong proficiency in FastAPI, Python, authentication mechanisms, and and ORM (Object-Relational Mapping)", 
+        "I have successfully contributed to and led projects focused on building robust and scalable web applications.",
+        "One notable project that showcases my skills involves the development of a FastAPI-based system with integrated authentication, ORM, and PostgreSQL database management.",
+      ],
+    },
+    {
+      title: "Software Developer and Tester",
+      company_name: "Meta",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "August 2023 - Present",
+      points: [
+        "As a Python Web Developer, I bring a wealth of experience in crafting robust and efficient web applications.",
+        "Proficient in utilizing PostgreSQL as a relational database, I have successfully designed and implemented scalable data models, ensuring optimal storage and retrieval of information.",
+        "My expertise extends to ReactJS, where I have played a pivotal role in developing dynamic and user-friendly interfaces that enhance the overall user experience.",
+        "In addition to my backend skills, I am well-versed in testing methodologies, employing Selenium for end-to-end testing and Postman for API testing.",
+        "This ensures the delivery of high-quality software with minimal bugs, contributing to a seamless and reliable user experience.",
+        "My commitment to writing clean and maintainable code is reflected in my collaborative approach, working seamlessly with cross-functional teams to achieve project objectives."
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
